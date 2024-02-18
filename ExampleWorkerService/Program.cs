a411c833e47913f4297d873f552acf57d7160ab7
Francisco de Santiago <jofrasa@hotmail.com>
2024-02-18 10:37:10 +0000

    Creation of Example Worker Service

---
+++ ExampleWorkerService/Program.cs
@@ -0,0 +1,10 @@
+using ExampleWorkerService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+	.ConfigureServices(services =>
+	{
+		services.AddHostedService<Worker>();
+	})
+	.Build();
+
+await host.RunAsync();
